74e7d662563ab86122d1ac0dfdf06c18c0bf5e90
Mehra-Jatin <jatinmehra578@gmail.com>
2025-06-15 23:34:50 +0530

    initial commit , Add:- DB Connection

---
+++ backend/controllers/authController.js
@@ -0,0 +1,12 @@
+export const login = async (req, res) => {
+   console.log("Login request received");
+};
+
+export const register = async (req, res) => {
+   console.log("Register request received");
+}
+
+export const logout = async (req, res) => {
+    console.log("logout");
+}
+
