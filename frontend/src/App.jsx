1c1366ea966bb34ea39fd7bd6dd7559164cf2f8d
Mehra-Jatin <jatinmehra578@gmail.com>
2025-06-22 16:54:09 +0530

    Add -components , Add -pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
-
-import React from 'react'
-
+import Navbar from './components/Navbar.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SettingsPage from './pages/SettingsPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/signup" element={<SignUpPage/>} />
+          <Route path="/login" element={<LoginPage/>} />
+           <Route path="/settings" element={<SettingsPage/>} />
+            <Route path="/profile" element={<ProfilePage/>} />
+        </Routes>
+      </Router>
     </div>
   )
 }
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+         home page
+    </div>
+  )
+}
+
+export default HomePage
