4003b00c6fac6b9298bfc409992f60bd4478d99b
Mehra-Jatin <jatinmehra578@gmail.com>
2025-06-24 16:53:25 +0530

    axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 
-const baseURL = import.meta.env.VITE_SERVER_URL
-  ? import.meta.env.VITE_SERVER_URL
-  : 'http://localhost:5000/api';
+const baseURL = import.meta.env.VITE_SERVER_URL || 'http://localhost:5000';
 
 export const axiosInstance = axios.create({
   baseURL: baseURL,
