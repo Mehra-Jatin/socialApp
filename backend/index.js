527529de62b42bddb29784b9c6e04f90ba689006
Jatin Mehra <145798017+Mehra-Jatin@users.noreply.github.com>
2025-08-06 21:00:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json({limit:'10mb'})); // Middleware to parse JSON bodies
 app.use(cookieParser()); // Middleware to parse cookies
 app.use(cors(
   {
-    origin: ['http://localhost:5173', process.env.CLIENT_URL], // Allow requests from these origins
+    origin: [process.env.CLIENT_URL], // Allow requests from these origins
     credentials: true, // Allow cookies to be sent with requests
   }
 ))
