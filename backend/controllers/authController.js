108aed4fe7a8d41fc3d19568191d4728802ee7ea
Jatin Mehra <145798017+Mehra-Jatin@users.noreply.github.com>
2025-08-06 21:14:37 +0530

    Update cookies

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -29,7 +29,6 @@ export const login = async (req, res) => {
         });
     
          res.cookie("token", token, {
-      httpOnly: true,
       secure: process.env.NODE_ENV === "production", // Use secure cookies in production
       sameSite : 'None',
       maxAge: 3600000, // 1 hour
@@ -83,7 +82,6 @@ export const register = async (req, res) => {
     });
 
     res.cookie("token", token, {
-      httpOnly: true,
       secure: process.env.NODE_ENV === "production", // Use secure cookies in production
       sameSite : 'None',
       maxAge: 3600000, // 1 hour
